b79b5198c0bbf93bb0753fef5fb45768c74f987d
arunwritings <arunkumarhr1102@gmail.com>
2025-09-24 23:29:42 +0530

    24-09-25

---
+++ src/main/java/datastructures/TwoSum_1.java
@@ -0,0 +1,26 @@
+package datastructures;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum_1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0;i< nums.length;i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TwoSum_1 twoSum1 = new TwoSum_1();
+        int[] nums = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum1.twoSum(nums, 18)));
+    }
+}
